f1d5257fee782d944a2eef88f4746be45b4fcf73
Ajaz Ahmad <iq.booster.2011@gmail.com>
2026-08-26 14:05:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from google import genai
 app = Flask(__name__)
 
 # Aapki Gemini API Key yahan dalo
-API_KEY = "YOUR_GEMINI_API_KEY_HERE"
+API_KEY = "AQ.Ab8RN6IYMosIUqVhjYK5pdrUoqjk94Fk0fJP6QPmw6gJVE1WNg"
 
 # Client initialize karo
 client = genai.Client(api_key=API_KEY)
